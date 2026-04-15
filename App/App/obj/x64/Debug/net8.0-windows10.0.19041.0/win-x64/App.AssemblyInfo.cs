e93746a16e8f3ec0d5cfb2c6a0fa48a0daf50398
martwijnman <153086626+martwijnman@users.noreply.github.com>
2026-04-15 15:01:02 +0200

    post

--- App/App/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/App.AssemblyInfo.cs
+++ App/App/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ed27e15825b6400760c9028b65401b8e4727b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
